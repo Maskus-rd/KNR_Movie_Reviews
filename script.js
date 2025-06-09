4f274a8e59ac26565d5707d38425f2a1ce720b05
Ryan <80864263+Maskus-rd@users.noreply.github.com>
2025-06-09 13:34:47 -0400

    Update script.js
    2/3 files being over hauled/entirely redone

--- script.js
+++ script.js
@@ -1,2 +1,59 @@
 
-//start of JS code for the blog
+//author: Ryan Dunhamn
+
+
+
+const homeBtn = document.querySelector("#home-button");
+const dataBtn = document.querySelector("#data-button");
+const newsBtn = document.querySelector("#news-button");
+const regBtn = document.querySelector("#register-button");
+const donateBtn = document.querySelector("#donate-button");
+const closeBtn = document.querySelector("#closePop");
+
+const homeCont = document.querySelector("#Home");
+const dataCont = document.querySelector("#Data");
+const newsCont = document.querySelector("#News");
+const popCont = document.querySelector("#popUp");
+
+
+//Screen Size Management
+
+//State Management
+
+const displayData = () => { //sould be one displaySwitch function
+
+    newsCont.style.display = "none";
+    homeCont.style.display = "none";
+    dataCont.style.display = "block";
+}
+
+const displayHome = () => { //sould be one displaySwitch function
+
+    newsCont.style.display = "none";
+    homeCont.style.display = "block";
+    dataCont.style.display = "none";
+}
+
+const displayNews = () => { //sould be one displaySwitch function
+
+    newsCont.style.display = "block";
+    homeCont.style.display = "none";
+    dataCont.style.display = "none";
+}
+
+const popUp = () => {
+
+    popCont.style.display = "block";
+}
+
+const closePop = () => {
+
+    popCont.style.display = "none";
+}
+
+homeBtn.addEventListener("click", displayHome);
+dataBtn.addEventListener("click", displayData);
+newsBtn.addEventListener("click", displayNews);
+donateBtn.addEventListener("click", popUp);
+regBtn.addEventListener("click", popUp);
+closeBtn.addEventListener("click", closePop);
