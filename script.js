8bca2a4d97f4c47fb00bce1b82e154689d846841
Ryan <80864263+Maskus-rd@users.noreply.github.com>
2025-06-09 13:38:20 -0400

    Update script.js
    Correcting the file uploaded from the overhaul.

--- script.js
+++ script.js
@@ -1,59 +1,121 @@
-
 //author: Ryan Dunhamn
 
+/*
+const ryanBtn = document.querySelector("#pivot-ryan");
+const ryanRating = document.querySelector("#rating-ryan");
 
 
+const ryanRotate = () => {
+
+    ryanRating.style.display("none");
+}
+
+ryanBtn.addEventListener("click", ryanRotate());
+
+*/
+
+//recent code below
+
+let title = null;
+let overallRating = 0;
+let ratingK = 0;
+let ratingN = 0;
+let ratingR = 0
+
+const titleField = document.querySelector("#new-title");
+const ratingFieldK = document.querySelector("#new-ratingK");
+const ratingFieldN = document.querySelector("#new-ratingN");
+const ratingFieldR = document.querySelector("#new-ratingR");
+
+const addReviewBtn = document.querySelector("#new-review-button");
+const testBtn = document.querySelector("#test-button");
+const saveDisBtn = document.querySelector("#save-display");
+const loginBtn = document.querySelector("#log-in-button");
 const homeBtn = document.querySelector("#home-button");
-const dataBtn = document.querySelector("#data-button");
-const newsBtn = document.querySelector("#news-button");
-const regBtn = document.querySelector("#register-button");
-const donateBtn = document.querySelector("#donate-button");
-const closeBtn = document.querySelector("#closePop");
 
-const homeCont = document.querySelector("#Home");
-const dataCont = document.querySelector("#Data");
-const newsCont = document.querySelector("#News");
-const popCont = document.querySelector("#popUp");
+const newReviewDisplay = document.querySelector("#new-review");
+const testDisplay = document.querySelector("#test-display");
+const homeDisplay = document.querySelector("#home");
+const loginDisplay = document.querySelector("#log-in");
 
 
-//Screen Size Management
+//Objects
 
-//State Management
+const smapleReview = {
 
-const displayData = () => { //sould be one displaySwitch function
-
-    newsCont.style.display = "none";
-    homeCont.style.display = "none";
-    dataCont.style.display = "block";
+    title: "Inception",
+    rating: 9.4,
+    kyleReview: {
+        score: 9
+    },
+    nickReview: {
+        score: 9.4
+    },
+    ryanReview: {
+        score: 9.8,
+        plot: 4,
+        acting: 3,
+        style: 2, 
+        feeling: 1,
+        quote: "Blowing the minds of youth everywhere."
+    }
 }
 
-const displayHome = () => { //sould be one displaySwitch function
 
-    newsCont.style.display = "none";
-    homeCont.style.display = "block";
-    dataCont.style.display = "none";
+const movieReview = {
+
+    constructor(title, rating){ //rating needs to be removed from the arg list
+
+        this.title = title;
+        this.rating = rating; //this line will need to be replace with an average formula grabing the values of the individual ratings.
+    }
 }
 
-const displayNews = () => { //sould be one displaySwitch function
 
-    newsCont.style.display = "block";
-    homeCont.style.display = "none";
-    dataCont.style.display = "none";
+
+
+
+const displayNewReview = () => {
+
+    newReviewDisplay.style.display = "block";
 }
 
-const popUp = () => {
+const testButton = () => {
 
-    popCont.style.display = "block";
+    testDisplay.innerHTML = "<h2 class='movie-title'>Test</h2> <h3 class='overall'>9.6</h3><div class='box'><h3 class='overall'>9.3</h3><h4 class='reviewer'>-Kyle</h4></div> <div class='box'><h3 class='overall'>9.5</h3>          <h4 class='reviewer'>-Nick</h4>   </div>        <div class='box'>     <h3 class='overall'>10</h3>            <h4 class='reviewer'>-Ryan</h4>        </div>        <button class='Eidt'>Edit</button>";
 }
 
-const closePop = () => {
+const saveDisplay = () => {  //This can be done via saving all the form data as an object. Look into formData.
 
-    popCont.style.display = "none";
+    //Step one save values into variables
+    title = titleField.value;
+    ratingK = ratingFieldK.value;
+    ratingN = ratingFieldN.value;
+    ratingR = ratingFieldR.value;
+    overallRating = (parseFloat(ratingK) + parseFloat(ratingN) + parseFloat(ratingR)) / 3; //make sure the math is right for the average formula
+    
+
+    //step two insert new html on to page using variable values
+    testDisplay.innerHTML = "<h2 class='movie-title'>" + title + "</h2><h3>Overall Rating: " + overallRating + "</h3></br><h4>Kyles Rating: " + ratingK + "</br><h4>Nicks Rating: " + ratingN + "</br><h4>Ryans Rating: " + ratingR;
+
+    //step three reset variable to be used again
+
 }
 
+const login = () => {
+
+    homeDisplay.style.display = "none";
+    loginDisplay.style.display = "block";
+}
+
+const displayHome = () => {
+
+    loginDisplay.style.display = "none";
+    homeDisplay.style.display = "block";
+}
+
+addReviewBtn.addEventListener("click", displayNewReview);
+testBtn.addEventListener("click", testButton);
+saveDisBtn.addEventListener("click", saveDisplay);
+loginBtn.addEventListener("click", login);
 homeBtn.addEventListener("click", displayHome);
-dataBtn.addEventListener("click", displayData);
-newsBtn.addEventListener("click", displayNews);
-donateBtn.addEventListener("click", popUp);
-regBtn.addEventListener("click", popUp);
-closeBtn.addEventListener("click", closePop);
